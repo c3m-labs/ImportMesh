7fcaec634729303f22b3dbf27516fd1adbd57739
PintarM <matevz.pintar@gmail.com>
2018-05-08 11:04:09 +0200

    Update PacletInfo.m with additional info about the package

--- PacletInfo.m
+++ PacletInfo.m
@@ -3,12 +3,14 @@
 (* Paclet Info File *)
 Paclet[
 	Name -> "ImportMesh",
-	Version -> "0.3.0",
+	Version -> "0.3.1",
 	WolframVersion -> "11.+",
     Description -> "Utilities for importing FEM meshes from other software.",
-    Creator -> "Pintar M, C3M",
+    Creator -> "info@c3m.si",
     Publisher->"C3M d.o.o.",
     URL -> "https://github.com/c3m-labs/ImportMesh",
+    Tags -> {"finite-elements","mesh","import"},
+    Categories -> {"FEM"},
 	Extensions -> {
 		{"Kernel", Root -> ".", Context ->{"ImportMesh`"}}
 	}
